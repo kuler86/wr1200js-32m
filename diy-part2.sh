2c667cf5af163e3f87427a9dcfcbbed574fc9976
kuler86 <79433417+kuler86@users.noreply.github.com>
2021-02-25 14:11:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,14 +14,14 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 获取luci-app-diskman和依赖
-mkdir -p package/diy-packages/luci-app-diskman && \
-mkdir -p package/diy-packages/parted && \
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/diy-packages/luci-app-diskman/Makefile
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/diy-packages/parted/Makefile
+#mkdir -p package/diy-packages/luci-app-diskman && \
+#mkdir -p package/diy-packages/parted && \
+#wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/diy-packages/luci-app-diskman/Makefile
+#wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/diy-packages/parted/Makefile
 
 # 获取hello world和依赖
-git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
 
  获取ssr-plus
 git clone https://github.com/kenzok8/openwrt-packages package/luci-app-ssr-plus
@@ -37,5 +37,5 @@ popd
 #=================================================
 
 # 拉取仓库
-git clone https://github.com/kenzok8/openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages
 
