c5958bd059ec8bd814f6193a552146e1b2d1f428
kuler86 <79433417+kuler86@users.noreply.github.com>
2021-02-25 09:08:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,26 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/openwrt-packages.git
+
+# 获取luci-app-diskman和依赖
+mkdir -p package/diy-packages/luci-app-diskman && \
+mkdir -p package/diy-packages/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/diy-packages/luci-app-diskman/Makefile
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/diy-packages/parted/Makefile
+
+# 获取hello world和依赖
+git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
+
+# 获取ssr-plus
+git clone https://github.com/kenzok8/openwrt-packages package/luci-app-ssr-plus
+
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+#=================================================
