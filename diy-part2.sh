eb222bdb460b8992b3da7c8fb42fc9b1a6399b8e
kuler86 <79433417+kuler86@users.noreply.github.com>
2021-03-01 22:24:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,8 @@ popd
 
 
 
-# 修改闪存为32m
+# 修改闪存为32m频率25MHz
 sed -i 's?define Device/youhua_wr1200js\r  IMAGE_SIZE := 16064k?define Device/youhua_wr1200js\r  IMAGE_SIZE := 32448k?g' target/linux/ramips/image/mt7621.mk
 sed -i 's?0xfb0000?0x1fb0000?g' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
+sed -i 's?spi-max-frequency = <10000000>?spi-max-frequency = <25000000>?g' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
 
